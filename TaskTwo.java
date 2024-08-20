6cce485fc757e6ef47b5ebe0d2b72261a08a4a43
Bolarji849 <mbolaji209@gmail.com>
2024-08-20 07:59:51 +0100

    Add TaskTwo file.java

---
+++ TaskTwo.java
@@ -0,0 +1,26 @@
+public class TaskTwo{
+
+	public static void main(String[] args){
+
+
+
+
+	int counter = 2 ;
+		
+	while ( counter <= 10 ){
+
+	System.out.println( counter);
+
+		counter = counter +2;
+		
+	}
+
+
+
+
+	}
+
+
+
+}
+
