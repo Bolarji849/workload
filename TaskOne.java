3c8a4b086a8880d08e95eef3dfdb279fd9ec86f0
Bolarji849 <mbolaji209@gmail.com>
2024-08-20 07:55:41 +0100

    Add TaskOne file

---
+++ TaskOne.java
@@ -0,0 +1,31 @@
+public class TaskOne{
+
+	public static void main(String[] args){
+
+
+
+
+	int counter = 1;
+		
+	while ( counter <= 10 ){
+
+	System.out.println( counter);c
+
+		counter++;
+
+	}
+
+
+
+
+
+
+
+	
+	}
+
+
+
+
+
+}
