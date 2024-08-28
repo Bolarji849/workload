7aedc5aef75496d41d3d1ce14a4b42dd5f3c47a3
Bolarji849 <mbolaji209@gmail.com>
2024-08-28 01:30:52 +0100

    PatternStyle

---
+++ Assessment/PatternStyle.java
@@ -0,0 +1,37 @@
+public class PatternStyle {
+
+	public static void main(String[] args){
+
+	
+
+
+
+
+	for(int count = 1; count <= 5 ; count++){
+
+			System.out.println(" ");
+
+		for ( int digit = 1; digit <= count ; digit++){
+		
+			System.out.print( " * ");
+
+		}
+			
+		}
+		
+	for(int count = 1; count < 5; count++){
+
+		System.out.println(" ");
+
+	  for( int digit = 4; digit >= count; digit--){
+
+			System.out.print( " * ");
+		
+	   }
+	   }
+
+
+}
+
+
+}
