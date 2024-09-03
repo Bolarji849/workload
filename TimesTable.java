96e34c42c88441e255609c95b6657a76099b5069
Bolarji849 <mbolaji209@gmail.com>
2024-09-03 07:17:14 +0100

    Times Table

---
+++ TimesTable.java
@@ -0,0 +1,23 @@
+public class TimesTable{
+
+	public static void main(String[] multuplication){
+
+
+	for(int counter = 1; counter <=9 ; counter++ ){
+	System.out.println("  ");
+	int digit = 1;
+	int count = 1;
+
+	while (digit <= 9){
+
+		count  = counter * digit;
+		System.out.print( counter+ "x" + digit + "=" + counter*digit + "   ");
+		digit++;
+		
+	}
+	System.out.println();	
+	}
+
+	}
+	
+}
