b350fa17fdb6b8c5a5b9541dd3ea279a5b5d4232
Bolarji849 <mbolaji209@gmail.com>
2024-09-03 07:13:22 +0100

    Numbers Divisibility

---
+++ NumbersDivisiblity.java
@@ -0,0 +1,39 @@
+public class NumbersDivisiblity {
+	public static void main(String[] args){
+
+
+	int counter = 0;
+	
+	for(int number = 100; number <= 200; number++){
+	
+
+	if(number % 3 == 0 || number % 4 == 0) counter++;
+	
+	System.out.print(number + "  ");
+
+	if ( counter %10 == 0) System.out.println();
+
+	
+	}
+
+
+
+
+
+	
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
