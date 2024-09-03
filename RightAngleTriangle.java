4337b0c926b568239b8fca1fc103756be000c41a
Bolarji849 <mbolaji209@gmail.com>
2024-09-03 07:06:59 +0100

    Right Angle Triangle

---
+++ RightAngleTriangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class RightAngleTriangle{
+	public static void main(String[] args){
+	Scanner inputCollection = new Scanner(System.in);
+
+
+	System.out.print("Enter the length of the base of the triangle");
+	int lengthOfBase = inputCollection.nextInt();
+
+
+	
+	for(int count = 1; count <= 10 ; count++){
+
+			System.out.println(" ");
+
+		for ( lengthOfBase = 1; lengthOfBase <= count ; lengthOfBase++){
+		
+			System.out.print( " * ");
+
+		}
+			
+		}
+		
+	
+
+  
+     }
+
+     }
